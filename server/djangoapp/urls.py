d6cd753f2ac268f9465bf57572ac1fe4b7a86645
Dominic <string.reverse@proton.me>
2024-08-15 21:25:34 -0400

    Added routing and logic for logout

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
+    # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
+
     # path for dealer reviews view
 
     # path for add a review view
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -36,9 +36,10 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    data = {"userName":""}
+    return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
