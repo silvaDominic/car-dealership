927882dd514e26439fc74066803fc9688fc81113
Dominic <string.reverse@proton.me>
2024-08-15 20:15:58 -0400

    Added paths for SPA portal

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -63,6 +63,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR,'frontend/static'),
+            os.path.join(BASE_DIR,'frontend/build'),
+            os.path.join(BASE_DIR,'frontend/build/static'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -138,5 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'frontend/static'),
+    os.path.join(BASE_DIR,'frontend/build'),
+    os.path.join(BASE_DIR,'frontend/build/static'),
 ]
 
