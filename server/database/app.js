2a6179168e510e438e78ba5a88a002f780b8d2e8
Dominic <string.reverse@proton.me>
2024-08-29 19:10:24 -0400

    Implemented Dealership endpoints

--- server/database/app.js
+++ server/database/app.js
@@ -25,7 +25,6 @@ try {
   Dealerships.deleteMany({}).then(()=>{
     Dealerships.insertMany(dealerships_data['dealerships']);
   });
-  
 } catch (error) {
   res.status(500).json({ error: 'Error fetching documents' });
 }
@@ -58,17 +57,32 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+  try  {
+    const data = await Dealerships.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({error: 'Error fetching dealerships.'});
+  }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+  try {
+    const data = await Dealerships.find({state: req.params.state});
+    res.json(data);
+  } catch(error) {
+    res.status(500).json({error: 'Error fetching dealerships.'});
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+  try {
+    const data = await Dealerships.find({id: req.params.id});
+    res.json(data);
+  } catch(error) {
+    res.status(500).json({error: 'Error fetching dealerships'});
+  }
 });
 
 //Express route to insert review
